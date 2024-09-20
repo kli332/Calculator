93882ae63e8f1b2a46f934cf6f9c5ef8547ff857
kli332 <kli332@wisc.edu>
2024-09-19 19:30:36 -0500

    operations

--- app/src/main/java/com/cs407/calculator/MainActivity.kt
+++ app/src/main/java/com/cs407/calculator/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.cs407.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,56 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val editText1 = findViewById<EditText>(R.id.editText1)
+        val editText2 = findViewById<EditText>(R.id.editText2)
+        val addition = findViewById<Button>(R.id.addition)
+        val subtraction = findViewById<Button>(R.id.subtraction)
+        val multiplication = findViewById<Button>(R.id.multiplication)
+        val division = findViewById<Button>(R.id.division)
+
+        addition.setOnClickListener {
+            val userInput1 = editText1.text.toString()
+            val userInput2 = editText2.text.toString()
+
+            val result = Integer.parseInt(userInput1) + Integer.parseInt(userInput2)
+
+            val intent = Intent(this, CalculatorActivity::class.java)
+            intent.putExtra("EXTRA_MESSAGE", result.toString())
+            startActivity(intent)
+        }
+
+        subtraction.setOnClickListener {
+            val userInput1 = editText1.text.toString()
+            val userInput2 = editText2.text.toString()
+
+            val result = Integer.parseInt(userInput1) - Integer.parseInt(userInput2)
+
+            val intent = Intent(this, CalculatorActivity::class.java)
+            intent.putExtra("EXTRA_MESSAGE", result.toString())
+            startActivity(intent)
+        }
+
+        multiplication.setOnClickListener {
+            val userInput1 = editText1.text.toString()
+            val userInput2 = editText2.text.toString()
+
+            val result = Integer.parseInt(userInput1) * Integer.parseInt(userInput2)
+
+            val intent = Intent(this, CalculatorActivity::class.java)
+            intent.putExtra("EXTRA_MESSAGE", result.toString())
+            startActivity(intent)
+        }
+
+        division.setOnClickListener {
+            val userInput1 = editText1.text.toString()
+            val userInput2 = editText2.text.toString()
+
+            val result = Integer.parseInt(userInput1) / Integer.parseInt(userInput2)
+
+            val intent = Intent(this, CalculatorActivity::class.java)
+            intent.putExtra("EXTRA_MESSAGE", result.toString())
+            startActivity(intent)
         }
     }
 }
